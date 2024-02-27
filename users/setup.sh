291ae646a28e9bac2d54f7a29931c42d2001a2a4
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2024-02-27 16:59:02 +0100

    add: notifications table

--- users/setup.sh
+++ users/setup.sh
@@ -2,9 +2,11 @@ source .env
 
 node src/users.js down $TEST
 node src/fcm_tokens.js down $TEST
+node src/notifications.js down $TEST
 
 node src/users.js up $TEST
 node src/fcm_tokens.js up $TEST
+node src/notifications.js up $TEST
 
 echo "schema up"
 
