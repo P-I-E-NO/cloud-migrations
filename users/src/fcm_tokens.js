20ee16ee930956c0674689f4d39cd3ec235ec556
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2024-03-01 11:31:10 +0100

    add: creation date in notifications

--- users/src/fcm_tokens.js
+++ users/src/fcm_tokens.js
@@ -1,63 +1,56 @@
 const { Client } = require("pg");
 
 (async () => {
+  // node migration_file.js up/down [test]
 
-    // node migration_file.js up/down [test]
+  const command = process.argv[2].toLowerCase();
 
-    const command = process.argv[2].toLowerCase();
+  const conn = new Client({
+    connectionString:
+      process.env.NODE_ENV === "test"
+        ? `postgresql://root@root_db:26257/test_db`
+        : process.env.DB_CONNECTION_STRING,
+  });
+  await conn.connect();
 
-    const conn = new Client({
-        connectionString: process.env.NODE_ENV === 'test' ? 
-            `postgresql://root@root_db:26257/test_db` :
-            process.env.DB_CONNECTION_STRING
-    });
-    await conn.connect();
-
-    try{
-
-        await conn.query("begin");
-        switch(command){
-
-            case "up":
-                
-                await conn.query(`
+  try {
+    await conn.query("begin");
+    switch (command) {
+      case "up":
+        await conn.query(`
                     create table if not exists fcm_tokens (
                         token text not null,
                         user_id varchar(32) not null,
                         primary key (token, user_id)
                     );
                 `);
-                    
-                await conn.query(`
+
+        await conn.query(`
                     alter table fcm_tokens add foreign key (user_id) references users(id)
-                `)
+                `);
 
-                await conn.query(`
+        await conn.query(`
                     create index fcm_fk_idx on fcm_tokens (user_id);
-                `)
+                `);
 
-                await conn.query("commit");
+        await conn.query("commit");
 
-            break;
-    
-            case "down":
-                
-                // await conn.query(`alter table users disable trigger all`);
-                await conn.query(`drop table if exists fcm_tokens cascade;`);
-                await conn.query("commit");
+        break;
 
-            break;
-    
-            default: throw Error('invalid command');
-    
-        }
+      case "down":
+        // await conn.query(`alter table users disable trigger all`);
+        await conn.query(`drop table if exists fcm_tokens cascade;`);
+        await conn.query("commit");
 
-    }catch(err){
-        console.log(err);
-        await conn.query("rollback");
-    }finally{
-        await conn.end();
+        break;
+
+      default:
+        throw Error("invalid command");
     }
-
-
+  } catch (err) {
+    console.log(err);
+    await conn.query("rollback");
+  } finally {
+    await conn.end();
+  }
 })();
--- users/src/notifications.js
+++ users/src/notifications.js
@@ -1,63 +1,57 @@
 const { Client } = require("pg");
 
 (async () => {
+  // node migration_file.js up/down [test]
 
-    // node migration_file.js up/down [test]
+  const command = process.argv[2].toLowerCase();
 
-    const command = process.argv[2].toLowerCase();
+  const conn = new Client({
+    connectionString:
+      process.env.NODE_ENV === "test"
+        ? `postgresql://root@root_db:26257/test_db`
+        : process.env.DB_CONNECTION_STRING,
+  });
+  await conn.connect();
 
-    const conn = new Client({
-        connectionString: process.env.NODE_ENV === 'test' ? 
-            `postgresql://root@root_db:26257/test_db` :
-            process.env.DB_CONNECTION_STRING
-    });
-    await conn.connect();
-
-    try{
-
-        await conn.query("begin");
-        switch(command){
-
-            case "up":
-                
-                await conn.query(`
+  try {
+    await conn.query("begin");
+    switch (command) {
+      case "up":
+        await conn.query(`
                     create table if not exists notifications (
                         id varchar(32) not null primary key,
                         to_user varchar(32) not null,
-                        data jsonb not null
+                        data jsonb not null,
+                        created_at timestamp not null default now()
                     );
                 `);
-                
-                await conn.query(`
+
+        await conn.query(`
                     alter table notifications add foreign key (to_user) references users(id)
-                `)
+                `);
 
-                await conn.query(`
+        await conn.query(`
                     create index notifications_fk on notifications (to_user);
-                `)
+                `);
 
-                await conn.query("commit");
+        await conn.query("commit");
 
-            break;
-    
-            case "down":
-                
-                // await conn.query(`alter table users disable trigger all`);
-                await conn.query(`drop table if exists notifications cascade;`);
-                await conn.query("commit");
+        break;
 
-            break;
-    
-            default: throw Error('invalid command');
-    
-        }
+      case "down":
+        // await conn.query(`alter table users disable trigger all`);
+        await conn.query(`drop table if exists notifications cascade;`);
+        await conn.query("commit");
 
-    }catch(err){
-        console.log(err);
-        await conn.query("rollback");
-    }finally{
-        await conn.end();
+        break;
+
+      default:
+        throw Error("invalid command");
     }
-
-
+  } catch (err) {
+    console.log(err);
+    await conn.query("rollback");
+  } finally {
+    await conn.end();
+  }
 })();
